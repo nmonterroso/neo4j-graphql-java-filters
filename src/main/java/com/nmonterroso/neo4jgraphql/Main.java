2fb08a1200da639f1a62e34d0ad5655ba46ad3b5
nmonterroso <nmonterroso@users.noreply.github.com>
2019-05-02 17:10:50 -0700

    initial commit

---
+++ src/main/java/com/nmonterroso/neo4jgraphql/Main.java
@@ -0,0 +1,45 @@
+package com.nmonterroso.neo4jgraphql;
+
+import graphql.schema.GraphQLSchema;
+import org.neo4j.graphql.SchemaBuilder;
+import org.neo4j.graphql.Translator;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+
+  private final String schemaDef;
+  private final GraphQLSchema schema;
+
+  private Main() throws IOException {
+    schemaDef = read("schemaDef.sdl");
+    schema = SchemaBuilder.buildSchema(schemaDef, new Translator.Context());
+  }
+
+  private void run() {
+    printQuery("query-friends-not-null.txt");
+    printQuery("query-friends-name-notnull.txt");
+    printQuery("query-empty-object.txt");
+  }
+
+  private void printQuery(String resource) {
+    try {
+      Translator.Cypher translation = new Translator(schema).translate(read(resource)).get(0);
+      System.out.println(String.format("%s:\n%s", resource, translation.getQuery()));
+    } catch (Exception e) {
+      System.out.println(String.format("ERROR: %s - %s", resource, e.getMessage()));
+    }
+  }
+
+  private static String read(String resource) throws IOException {
+    ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    File file = new File(classLoader.getResource(resource).getFile());
+    return new String(Files.readAllBytes(file.toPath()));
+  }
+
+  public static void main(String[] args) throws IOException {
+    new Main().run();
+  }
+}
